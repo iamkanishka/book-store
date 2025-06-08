b51b618cf49fb8e9a352861503ffe9cc7cfe9263
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-08 16:39:03 +0530

    Added shipping-end app under be

---
+++ apps/be/shipping-end-api-e2e/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'shipping-end-api-e2e',
+  preset: '../../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/shipping-end-api-e2e',
+};
