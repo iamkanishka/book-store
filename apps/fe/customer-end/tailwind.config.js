ac1cc5da3387a553f4827673f4aa5b8415c6fff0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-11 18:43:07 +0530

    Added tailwind  for cutomer app

---
+++ apps/fe/customer-end/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.{html,ts}",
+    "./apps/fe/customer-end/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
