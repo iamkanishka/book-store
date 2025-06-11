8b1ff7ad999ff364e09843d424fa2a6e47df0eed
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-11 18:55:35 +0530

    Added tailwind config  for inventory  app

---
+++ apps/fe/inventory-end/tailwind.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./apps/fe/inventory-end/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
