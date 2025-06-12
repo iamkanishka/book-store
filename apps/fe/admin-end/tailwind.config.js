02bd6efbb7c5f7c6e1d46aa9979506cec4f957d1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-12 14:13:09 +0530

    Added tailwind config

---
+++ apps/fe/admin-end/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.{html,ts}",
+    "./apps/fe/admin-end/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
