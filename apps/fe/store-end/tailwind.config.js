c00b2cd0795f98f288f220eae8667e92e9e8bf1e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-12 14:23:29 +0530

    Added tailwind config

---
+++ apps/fe/store-end/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.{html,ts}",
+    "./apps/fe/store-end/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
