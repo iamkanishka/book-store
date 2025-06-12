3e1e5eaa7dc9d40f0fbbd5b599f933899ea55b9a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-12 14:21:49 +0530

    Added tailwind config

---
+++ apps/fe/shipping-end/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.{html,ts}",
+    "./apps/fe/shipping-end/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
