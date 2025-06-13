3c5b3c1aae437aa194a7c6ebe16340684ec547a4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-13 11:31:34 +0530

    chore:  imported header, sidebar, backdrop and inject SidebarService

--- apps/fe/admin-end/src/app/app.component.ts
+++ apps/fe/admin-end/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { SidebarService } from '../services/sidebar/sidebar.service';
+import { HeaderComponent } from '../components/header/header.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { BackdropComponent } from '../components/backdrop/backdrop.component';
+
+
+ 
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ RouterModule, HeaderComponent, SidebarComponent, BackdropComponent],
+  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'admin-end';
+  sidebar = inject(SidebarService);
 }
